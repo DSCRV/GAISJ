f4942ca046bafcdc883ba90809687342752ece3a
prabhav5112 <prabhavbk5112@gmail.com>
2024-05-11 11:52:44 +0530

    Add leaderboard data for May 11th

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    fetch('data/GenAI_May10.csv')
+    fetch('data/GenAI_May11.csv')
         .then(response => response.text())
         .then(data => {
             const rows = data.split('\n');

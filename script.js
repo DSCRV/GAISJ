516ddd23e4d2d0b09eb5f46bcef6c281292f1ace
prabhav5112 <prabhavbk5112@gmail.com>
2024-05-23 19:18:07 +0530

    Update leaderboard data for May 23rd

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    fetch('data/GenAI_May22.csv')
+    fetch('data/GenAI_May23.csv')
         .then(response => response.text())
         .then(data => {
             const rows = data.split('\n');
